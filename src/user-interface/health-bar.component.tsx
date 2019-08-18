b54c605332b5334ac2b9320f289ac737515595d8
JESShEart <jeshrt@protonmail.com>
2019-08-17 23:16:47 -0400

    styled the health bar some

--- src/user-interface/health-bar.component.tsx
+++ src/user-interface/health-bar.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Character from '../engine/character';
+import './health-bar.component.css';
 
 interface IProps {
   character: Character;
@@ -8,10 +9,16 @@ interface IProps {
 export default class HealthBarComponent extends React.Component<IProps> {
   public render() {
     return (
-      <progress
-        max={this.props.character.maxHealth}
-        value={this.props.character.health}
-      />
+      <div>
+        <progress
+          className='health-bar'
+          max={this.props.character.maxHealth}
+          value={this.props.character.health}
+        />
+        <div className='health-text'>
+          {this.props.character.health}/{this.props.character.maxHealth} HP
+        </div>
+      </div>
     );
   }
 }
