696742aad11b3ec4659817cdcb84333c16493883
JESShEart <jeshrt@protonmail.com>
2019-08-17 20:38:14 -0400

    updated effect interface to be less confusing looking

--- src/engine/effect/effect.ts
+++ src/engine/effect/effect.ts
@@ -1,7 +1,5 @@
 import Character from '../character';
 
-export type Effect = (target: Character) => void;
-
 export default interface IEffect {
-  activate: Effect;
+  activate: (target: Character) => void;
 }
